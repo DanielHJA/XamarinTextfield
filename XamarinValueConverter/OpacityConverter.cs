f54a80d110e39a41b4d6c9c1dad9e3a8ee79e3f9
Daniel <daniel.hjartstrom@gmail.com>
2020-01-23 12:55:15 +0100

    Enable + disable button and using value converter

---
+++ XamarinValueConverter/OpacityConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace XamarinValueConverter
+{
+    public class OpacityConverter : IValueConverter
+    {
+
+        public double True { get; set; }
+
+        public double False { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return False;
+            }
+            if (value.GetType() != typeof(bool))
+            {
+                return False;
+            }
+            return ((bool)value) ? True : False;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+
+    }
+}
